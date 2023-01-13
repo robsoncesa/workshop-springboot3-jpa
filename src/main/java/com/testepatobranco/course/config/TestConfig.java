bbee4c50cef320f74678e0dde1af4b01251fa1c7
Robson Cesa <robsoncesa@gmail.com>
2023-01-13 16:27:37 -0300

    aula 11

--- src/main/java/com/testepatobranco/course/config/TestConfig.java
+++ src/main/java/com/testepatobranco/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.testepatobranco.course.entities.Category;
 import com.testepatobranco.course.entities.Order;
+import com.testepatobranco.course.entities.OrderItem;
 import com.testepatobranco.course.entities.Product;
 import com.testepatobranco.course.entities.User;
 import com.testepatobranco.course.entities.enums.OrderStatus;
 import com.testepatobranco.course.repositories.CategoryRepository;
+import com.testepatobranco.course.repositories.OrderItemRepository;
 import com.testepatobranco.course.repositories.OrderRepository;
 import com.testepatobranco.course.repositories.ProductRepository;
 import com.testepatobranco.course.repositories.UserRepository;
@@ -33,6 +35,10 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +74,14 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 		
 	}
 }
---
+++ src/main/java/com/testepatobranco/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.testepatobranco.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.testepatobranco.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
